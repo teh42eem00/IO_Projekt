8edb877ffa095e27004eb73f7af35afe83597a4c
Tomasz Macałka <tomasz.macalka@student.pk.edu.pl>
2022-04-19 23:57:10 +0200

    first django app - polling app test

--- car_expenses_app/car_expenses_app/urls.py
+++ car_expenses_app/car_expenses_app/urls.py
@@ -1,4 +1,4 @@
-"""car_expenses URL Configuration
+"""car_expenses_app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
