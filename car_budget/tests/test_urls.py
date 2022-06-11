c74669d24ab22d1b8c139d61c6d24500378624f5
Tomasz Macałka <tomasz.macalka@student.pk.edu.pl>
2022-06-11 19:12:28 +0200

    Implementation of first unit django tests (URLs), added CI step to run them

---
+++ car_budget/tests/test_urls.py
@@ -0,0 +1,16 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from car_budget.views import car_list, car_detail, CarCreateView
+
+class TestUrls(SimpleTestCase):
+    def test_list_resolving(self):
+        url = reverse('list')
+        self.assertEquals(resolve(url).func, car_list)
+
+    def test_car_create_resolving(self):
+        url = reverse('add')
+        self.assertEquals(resolve(url).func.view_class, CarCreateView)
+
+    def test_car_detail_resolving(self):
+        url = reverse('detail', args=['some-slug'])
+        self.assertEquals(resolve(url).func, car_detail)
